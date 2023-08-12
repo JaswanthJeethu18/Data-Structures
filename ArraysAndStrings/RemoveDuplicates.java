2f6c3da3f20191e7bece4f292a4e55442e08dc57
JaswanthJeethu18 <jaswanth18jeethu@gmail.com>
2023-08-12 06:36:58 +0530

    removeDuplicates file add

--- ArraysAndStrings/RemoveDuplicates.java
+++ ArraysAndStrings/RemoveDuplicates.java
@@ -4,14 +4,16 @@ public class RemoveDuplicates {
     public static void main(String[] args) {
         int[] arr = {1,1,1,2,2,2,3,3,3,4,4,4};
         int ans = removeDuplicates(arr);
+        System.out.println(ans);
     }
 
     public static int removeDuplicates(int[] arr){
         int i = 0 ;
         for(int j=1 ; j<arr.length;j++){
             if(arr[j] != arr[i]){
-                arr[i+1] = a[j];
+                arr[i+1] = arr[j];
                 i++;
+
             }
         }
         return (i+1);
